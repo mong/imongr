555a98b25236905bd383f5e591c9e782864f314d
Are Edvardsen <biorakel@gmail.com>
2020-03-30 10:27:56 +0200

    replace latin1 codes

--- data-raw/data.R
+++ data-raw/data.R
@@ -8,4 +8,12 @@ data <- cbind(data, delivery_id=rep(1, dim(data)[1]))
 ind <- is.na(data$OrgNrShus)
 data <- data[!ind, ]
 
+# substitute some strange codes (latin1?) with proper chars
+data$ShNavn <- gsub("\xe6", "æ", data$ShNavn)
+data$ShNavn <- gsub("\xc6", "Æ", data$ShNavn)
+data$ShNavn <- gsub("\xf8", "ø", data$ShNavn)
+data$ShNavn <- gsub("\xd8", "Ø", data$ShNavn)
+data$ShNavn <- gsub("\xe5", "å", data$ShNavn)
+data$ShNavn <- gsub("\xc5", "Å", data$ShNavn)
+
 usethis::use_data(data, overwrite = TRUE)
