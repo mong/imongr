48f2ce02187b5429eb97e64114e431c0e0c475aa
Are Edvardsen <biorakel@gmail.com>
2020-03-30 10:29:08 +0200

    space

--- data-raw/user.R
+++ data-raw/user.R
@@ -1,6 +1,6 @@
 ## code to prepare `user` dataset goes here
 
-user <- data.frame(user_name="mongr", name="Mongr No",phone="+4747474747",
+user <- data.frame(user_name="mongr", name="Mongr No", phone="+4747474747",
                    email="jesus@sky.com", valid=1, org_id=1)
 
 usethis::use_data(user)
