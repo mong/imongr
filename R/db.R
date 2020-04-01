a41bcfc2f71ddd47ca6a70b23a81b3d11e874227
Are Edvardsen <biorakel@gmail.com>
2020-04-01 10:28:29 +0200

    update docs

--- R/db.R
+++ R/db.R
@@ -1,16 +1,25 @@
-#' Database functions for imongr
+#' Low level database functions for imongr
 #'
-#' Database credentials are read from config. If one or more of these are
+#' Database metadata are read from config. If one or more of these are
 #' defined 'env' corresponding values will be fetched from the environmental
-#' variables IMONGR_DB_USER and IMONGR_DB_PASS. If these are undefined the
-#' function providing connection handles will exit with an error.
+#' variables IMONGR_DB_HOST, IMONGR_DB_NAME, IMONGR_DB_USER and IMONGR_DB_PASS.
+#' If these are undefined the function providing connection handles will exit
+#' with an error. What the function expects from table names and variable
+#' names in the data is also defined by the configuration. Thus, functions can
+#' be left unchanged when the data model is altered as long as such changes
+#' are reflected by configuration.
+#'
+#' Ordianry user interactions with data should have their own functions, but
+#' may be built ontop of these ones. For instance, such functions must make
+#' sure consistency (\emph{e.g.} foreign keys) between database tables are
+#' kept in order.
 #'
 #' @param pool a database connection pool object
 #' @param table string defining target database table
 #' @param df data frame containing data to be inserted into a database
 #' @return Database pool object, data frame or status message
 #' @name db
-#' @aliases make_pool insert_tab get_data
+#' @aliases make_pool drain_pool insert_tab get_data
 NULL
 
 #' @rdname db
