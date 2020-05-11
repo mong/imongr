e749d9eaef0d31755659d884e9ae148a155e25c2
Are Edvardsen <biorakel@gmail.com>
2020-05-11 15:42:04 +0200

    might come in handy

--- data-raw/org.R
+++ data-raw/org.R
@@ -2,4 +2,25 @@
 
 org <- qmongrdata::SykehusNavnStruktur
 
+# stranger_strings <- function(data, vars) {
+#
+#   subs <- function(vec) {
+#     vec <- gsub("\xe6", "æ", vec)
+#     vec <- gsub("\xc6", "Æ", vec)
+#     vec <- gsub("\xf8", "ø", vec)
+#     vec <- gsub("\xd8", "Ø", vec)
+#     vec <- gsub("\xe5", "å", vec)
+#     vec <- gsub("\xc5", "Å", vec)
+#   }
+#
+#   for (i in seq_len(length(vars))) {
+#     data[vars[i]] <- subs(data[vars[i]])
+#   }
+#   data
+# }
+#
+# vars <- c("RHF", "HF", "Hfkortnavn", "OrgNavnEnhetsreg", "SykehusnavnLang",
+#           "SykehusNavn")
+# org <- stranger_strings(org, vars)
+
 usethis::use_data(org, overwrite = TRUE)
