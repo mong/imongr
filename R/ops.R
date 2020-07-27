2945dc6b1bbc246b9c1d66293af5cc6ab4b2672a
arnfinn <arnfinn.steindal@gmail.com>
2020-07-27 19:00:47 +0200

    Remove lintr warnings
    Added nolint behind lines where PascalCase are used (variable names).
    
    Should consider renaming OrgNr, KvalIndID etc.

--- R/ops.R
+++ R/ops.R
@@ -101,9 +101,9 @@ ORDER BY
   df <- pool::dbGetQuery(pool, query)
 
   # timestamp in db is UTC, convert back to "our" time zone
-  df$Dato <- format(df$Dato, format = conf$app_text$format$date,
+  df$Dato <- format(df$Dato, format = conf$app_text$format$date, # nolint
                     tz = Sys.getenv("TZ"))
-  df$Tid <- format(df$Tid, format = conf$app_text$format$time,
+  df$Tid <- format(df$Tid, format = conf$app_text$format$time, # nolint
                    tz = Sys.getenv("TZ"))
 
   df
