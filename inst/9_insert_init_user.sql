7c9da92f020faf3f89802e0127696cb438aa3661
Are Edvardsen <biorakel@gmail.com>
2020-08-12 14:55:48 +0200

    minor fix of init username for next ver

--- inst/9_insert_init_user.sql
+++ inst/9_insert_init_user.sql
@@ -1,2 +1,2 @@
 INSERT INTO `user` (`id`, `user_name`, `name`, `phone`, `email`, `valid`) VALUES
-(1,	'mongr',	'Mongr No',	'+4747474747',	'jesus@sky.com',	1);
+(1,	'imongr@mongr.no',	'Mongr No',	'+4747474747',	'imongr@mongr.no',	1);
