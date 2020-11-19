f91b323b2c69704a7e35419744324a4d6e3a18a9
arnfinn <arnfinn.steindal@gmail.com>
2020-11-19 14:58:53 +0100

    Added test to find out if bug has been fixed

---
+++ tests/testthat/test-diabetes.R
@@ -0,0 +1,15 @@
+# Test created because of issue #75
+# https://github.com/mong/imongr/issues/75
+#
+# Will fail until this bug has been fixed
+
+test_that("Has issue 75 been fixed", {
+  diabetes <- csv_to_df("data/diabetes_voksne.csv", sep = ";", dec = ",")
+  testthat::expect_equal_to_reference(diabetes, "data/diabetes.rda")
+
+  ind_descr <- readRDS(file = "data/ind.rds")
+  org_structure <- readRDS(file = "data/org.rds")
+
+  testthat::expect_equal_to_reference(agg(diabetes, org_structure, ind_descr), "data/diabetes_agg.rda")
+
+})
