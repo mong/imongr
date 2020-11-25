bf9fa86429fbbb10b20f7a650428abf1c52ec003
arnfinn <arnfinn.steindal@gmail.com>
2020-11-25 16:16:30 +0100

    Define some enc in test

--- tests/testthat/test-diabetes.R
+++ tests/testthat/test-diabetes.R
@@ -10,7 +10,13 @@ test_that("Has issue 75 been fixed", {
 
   diabetes <- readRDS(file = "data/diabetes.rds")
   ind_descr <- readRDS(file = "data/ind.rds")
+  Encoding(ind_descr$title) <- "UTF-8"
+  Encoding(ind_descr$short_description) <- "UTF-8"
+  Encoding(ind_descr$long_description) <- "UTF-8"
   org_structure <- readRDS(file = "data/org.rds")
+  Encoding(org_structure$hospital) <- "UTF-8"
+  Encoding(org_structure$hf) <- "UTF-8"
+  Encoding(org_structure$rhf) <- "UTF-8"
   all_orgnr <- readRDS(file = "data/all_orgnr.rds")
 
   diabetes <- dplyr::left_join(diabetes, all_orgnr, by = "orgnr")
