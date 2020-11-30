a9500cb59138fa3b644c9fa0b6e8a7abe05f044a
arnfinn <arnfinn.steindal@gmail.com>
2020-11-30 09:21:09 +0100

    reintroduce test of csv_to_df

--- tests/testthat/test-diabetes.R
+++ tests/testthat/test-diabetes.R
@@ -3,11 +3,12 @@
 #
 # Will fail until this bug has been fixed
 
-test_that("Has issue 75 been fixed", {
-#  diabetes <- csv_to_df("data/diabetes_voksne.csv", sep = ";", dec = ",")
+test_that("Test csv_to_df() with file", {
+  diabetes <- csv_to_df("data/diabetes_voksne.csv", sep = ";", dec = ",")
+  testthat::expect_equal_to_reference(diabetes, "data/diabetes_csv.rda")
+})
 
-#  testthat::expect_equal_to_reference(diabetes, "data/diabetes.rda")
-
+test_that("Test agg() with files", {
   diabetes <- readRDS(file = "data/diabetes.rds")
   ind_descr <- readRDS(file = "data/ind.rds")
   Encoding(ind_descr$title) <- "UTF-8"
@@ -22,7 +23,6 @@ test_that("Has issue 75 been fixed", {
   diabetes <- dplyr::left_join(diabetes, all_orgnr, by = "orgnr")
   testthat::expect_equal_to_reference(diabetes, "data/diabetes.rda")
   diabetes_agg <- agg(diabetes, org_structure, ind_descr)
-  print(diabetes_agg)
   testthat::expect_equal_to_reference(diabetes_agg, "data/diabetes_agg.rda")
 
 })
