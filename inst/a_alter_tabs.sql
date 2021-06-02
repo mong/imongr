f5d77d47a495070fd4d2c2d8774b6fda16e412c6
Are Edvardsen <biorakel@gmail.com>
2021-06-02 10:53:34 +0000

    db changes

--- inst/a_alter_tabs.sql
+++ inst/a_alter_tabs.sql
@@ -10,6 +10,12 @@ ADD COLUMN IF NOT EXISTS
   `context` varchar(127) NOT NULL DEFAULT 'caregiver'
 AFTER `unit_name`;
 
+ALTER TABLE
+  `agg_data`
+ADD COLUMN IF NOT EXISTS
+  `delivery_time` timestamp NULL DEFAULT NULL
+AFTER `dg`;
+
 CREATE INDEX IF NOT EXISTS index_data_context ON data (context);
 
 CREATE INDEX IF NOT EXISTS index_agg_data_context ON agg_data (context);
