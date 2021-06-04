96fb648ae69c428d9bff3cdc4c5c408e084e7fe3
arnfinn <arnfinn.steindal@gmail.com>
2021-06-04 13:09:34 +0200

    Test that will fail until small number round is fixed

---
+++ tests/testthat/test-get_indicator_level.R
@@ -0,0 +1,9 @@
+context("get_indicator_level")
+
+test_that("get_indicator_level() is working with realistic input", {
+  data <- readRDS(file = "data/get_indicator_level_gdf.rds")
+  descr <- readRDS(file = "data/get_indicator_level_ind.rds")
+
+  testthat::expect_equal_to_reference(get_indicator_level(gdf = data, ind = descr),
+                                      "data/get_indicator_level_ref.rda")
+})
