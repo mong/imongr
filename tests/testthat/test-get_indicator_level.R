d31d9f66c1700240854b8fab0e5f8140903c8cce
arnfinn <arnfinn.steindal@gmail.com>
2021-06-07 09:26:40 +0200

    lintr

--- tests/testthat/test-get_indicator_level.R
+++ tests/testthat/test-get_indicator_level.R
@@ -4,6 +4,7 @@ test_that("get_indicator_level() is working with realistic input", {
   data <- readRDS(file = "data/get_indicator_level_gdf.rds")
   descr <- readRDS(file = "data/get_indicator_level_ind.rds")
 
-  testthat::expect_equal_to_reference(get_indicator_level(gdf = data, ind = descr),
+  testthat::expect_equal_to_reference(get_indicator_level(gdf = data,
+                                                          ind = descr),
                                       "data/get_indicator_level_ref.rds")
 })
