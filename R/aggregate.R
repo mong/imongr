8ff75d2339a61c8efbb4c70d546a4a2f202b9b96
arnfinn <arnfinn.steindal@gmail.com>
2022-12-21 14:10:56 +0100

    dplyr::all_of

--- R/aggregate.R
+++ R/aggregate.R
@@ -234,7 +234,7 @@ agg_from_level <- function(df, org, ind, conf, from_level) {
                             denominator = sum(.data$denominator))
     idf$unit_level <- rep(unit_levels[i], dim(idf)[1])
     this_org <- org %>%
-      dplyr::select(all_of(groups[i]), all_of(unit_levels[i])) %>%
+      dplyr::select(dplyr::all_of(groups[i]), dplyr::all_of(unit_levels[i])) %>%
       dplyr::distinct()
     idf <- dplyr::left_join(idf, this_org, by = groups[i])
     names(idf)[names(idf) == groups[i]] <- "orgnr"
