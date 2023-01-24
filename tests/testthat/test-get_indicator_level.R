11f6fb1aad9b40ea7c770f691a4f215cfa43011c
arnfinn <arnfinn.steindal@gmail.com>
2023-01-24 09:33:25 +0100

    simplify test

--- tests/testthat/test-get_indicator_level.R
+++ tests/testthat/test-get_indicator_level.R
@@ -2,9 +2,10 @@ context("get_indicator_level")
 
 test_that("get_indicator_level() is working with realistic input", {
   data <- readRDS(file = "data/get_indicator_level_gdf.rds")
-  descr <- readRDS(file = "data/get_indicator_level_ind.rds")
+  ref_data <- data
+  ref_data$level <- ""
+  ref_data$level_direction <- NA
 
-  testthat::expect_equal_to_reference(get_indicator_level(gdf = data,
-                                                          ind = descr),
-                                      "data/get_indicator_level_ref.rds")
+  testthat::expect_equal(get_indicator_level(gdf = data, ind = data.frame()),
+                         ref_data)
 })
