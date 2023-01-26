d656ced8b37a267f675cf40a219cc61927a754a0
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2023-01-26 13:06:58 +0100

    Remove choose encoding option (#262)

--- tests/testthat/test-mod.R
+++ tests/testthat/test-mod.R
@@ -325,20 +325,13 @@ test_that("upload module has output...", {
         registry = 10,
         sep = ";",
         dec_sep = ",",
-        enc = "UTF-8",
-        other_encoding = "MS-ANSI",
         sample_size = 20,
         sample_type = TRUE,
         latest_update = "2022-12-24",
         latest_affirm = "2022-01-01"
       )
-      expect_equal(encoding(), "UTF-8")
       expect_equal_to_reference(ind(), "data/norgast_ind.rds")
       expect_equal(df(), data.frame())
-      session$setInputs(
-        enc = "Annet",
-      )
-      expect_equal(encoding(), "MS-ANSI")
     }
   )
 })
