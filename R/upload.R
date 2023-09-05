bb85652ea241e9f0d6b313b9e657414fa748236b
arnfinn <arnfinn.steindal@gmail.com>
2023-09-05 15:54:11 +0200

    some lintr

--- R/upload.R
+++ R/upload.R
@@ -336,8 +336,7 @@ check_duplicated_inds <- function(registry, df, ind, conf, pool) {
   fail <- FALSE
   report <- ""
 
-  if ("ind_id" %in% names(df) &&
-      "orgnr" %in% names(df)) {
+  if ("ind_id" %in% names(df) && "orgnr" %in% names(df)) {
 
     ind_id_type <- data.frame(ind_id = ind$id, type = ind$type)
     orgnr <- get_all_orgnr(pool)
@@ -346,11 +345,11 @@ check_duplicated_inds <- function(registry, df, ind, conf, pool) {
     df_duplicated <- df %>%
       dplyr::left_join(ind_id_type, by = "ind_id") %>%
       dplyr::left_join(orgnr, by = "orgnr") %>%
-      dplyr::filter(!type %in% conf$var$fraction$type) %>%
-      dplyr::select(ind_id, orgnr, context, unit_level, year) %>%
+      dplyr::filter(!.data$type %in% conf$var$fraction$type) %>%
+      dplyr::select(.data$ind_id, .data$orgnr, .data$context, .data$unit_level, .data$year) %>%
       dplyr::group_by_all() %>%
       dplyr::count() %>%
-      dplyr::filter(n > 1)
+      dplyr::filter(.data$n > 1)
 
     if (nrow(df_duplicated) > 0) {
       fail <- TRUE
