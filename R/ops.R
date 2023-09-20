1a19c3d736d03e3a1025a432b7f23f78d8e8e53d
stein-kato <steinkato.lindberg@yahoo.no>
2023-09-20 13:56:57 +0200

    Fikset filtrering av data i publisering

--- R/ops.R
+++ R/ops.R
@@ -104,12 +104,12 @@ insert_data_prod <- function(pool_verify, pool_prod, df, registry_delivery_ids, 
   reg_id <- ind$registry_id[1]
 
   # Deliveries to be published
-  # Get all deliverie with published = 0 that are referenced
+  # Get all deliveries with published = 0 that are referenced
   # in the data table for the current registry
-  new_deliveries_and_inds <- pool::dbGetQuery(
+  filter_values <- pool::dbGetQuery(
     pool_verify,
     paste(
-      "SELECT DISTINCT data.delivery_id, data.ind_id
+      "SELECT DISTINCT data.delivery_id, data.ind_id, data.context, data.year
      FROM data LEFT JOIN ind ON data.ind_id = ind.id
      LEFT JOIN delivery ON data.delivery_id = delivery.id
      WHERE ind.registry_id =", reg_id,
@@ -119,7 +119,7 @@ insert_data_prod <- function(pool_verify, pool_prod, df, registry_delivery_ids, 
   )
 
   # To iterate over
-  new_delivery_ids <- unique(new_deliveries_and_inds$delivery_id)
+  new_delivery_ids <- unique(filter_values$delivery_id)
 
   # New row in the publish table in prod
   new_publish <- data.frame(
@@ -143,9 +143,19 @@ insert_data_prod <- function(pool_verify, pool_prod, df, registry_delivery_ids, 
 
   # Iterate over deliveries and insert data into prod
   for (delivery_id_i in new_delivery_ids) {
-    # Filter data to include only the indicators uploaded in the delivery
-    inds_i <- new_deliveries_and_inds$ind_id[new_deliveries_and_inds$delivery_id == delivery_id_i]
-    df_i <- df[df$ind_id %in% inds_i, ]
+
+    # Filter data to include only the lines uploaded in the delivery
+    filter_values_i <- filter_values[filter_values$delivery_id == delivery_id_i, ]
+
+    # Indicator id, context and year are pasted into strings and compared
+    filter_vector_i <- paste0(filter_values_i$ind_id, filter_values_i$context, filter_values_i$year)
+
+    keep_inds <- sapply(paste0(df$ind_id, df$context, df$year),
+                        FUN = function(x) {
+                          x %in% filter_vector_i
+                        })
+
+    df_i <- df[keep_inds, ]
 
     ##### In production #####
     delivery_i <- pool::dbGetQuery(
