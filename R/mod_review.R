3671f7bc642bb37c7d72259a8ce630e82ca0db4a
stein-kato <steinkato.lindberg@yahoo.no>
2024-03-19 14:16:56 +0100

    Nå med data

--- R/mod_review.R
+++ R/mod_review.R
@@ -368,7 +368,9 @@ review_server <- function(id, registry_tracker, pool) {
     output$download <- shiny::downloadHandler(
       filename = "Test.csv",
       content = function(file_name) {
-        write.csv(data.frame(a = c(1,2,3), b = c(4,5,6)), file_name)
+        browser()
+        dat <- pool::dbGetQuery(pool, paste0("SELECT * FROM evaluation WHERE registry_id = ", input$selected_registry))
+        write.csv(dat, file_name)
       }
     )
 
