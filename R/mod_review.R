0e2fa7ea0e298b0d1f533eb66206bfce8a2bda2f
arnfinn <arnfinn.steindal@gmail.com>
2024-03-19 15:44:23 +0100

    Ignorer noen lintr-feil

--- R/mod_review.R
+++ R/mod_review.R
@@ -517,9 +517,9 @@ review_server <- function(id, registry_tracker, pool) {
         names(colour_map) <- c("A", "B", "C", NA)
 
         if (!input$show_dg_plot) {
-          ggplot2::ggplot(data = plot_data, ggplot2::aes(x = year, y = stage)) +
+          ggplot2::ggplot(data = plot_data, ggplot2::aes(x = year, y = stage)) + # nolint: object_usage_linter.
             ggplot2::geom_line() +
-            ggplot2::geom_point(ggplot2::aes(colour = factor(level)), size = 3) +
+            ggplot2::geom_point(ggplot2::aes(colour = factor(level)), size = 3) + # nolint: object_usage_linter.
             ggplot2::scale_x_continuous("År", 2013:get_last_year(), limits = c(2013, get_last_year())) +
             ggplot2::scale_y_continuous("Stadium", 1:4, limits = c(1, 4)) +
             ggplot2::scale_colour_manual(values = colour_map, limits = c("A", "B", "C", NA)) +
@@ -527,9 +527,9 @@ review_server <- function(id, registry_tracker, pool) {
             ggplot2::theme(text = ggplot2::element_text(size = 14)) +
             ggplot2::guides(colour = ggplot2::guide_legend(title = "Niv\u00e5"))
         } else {
-          ggplot2::ggplot(data = plot_data, ggplot2::aes(x = year, y = reported_dg)) +
+          ggplot2::ggplot(data = plot_data, ggplot2::aes(x = year, y = reported_dg)) + # nolint: object_usage_linter.
             ggplot2::geom_line() +
-            ggplot2::geom_point(ggplot2::aes(colour = factor(level)), size = 3) +
+            ggplot2::geom_point(ggplot2::aes(colour = factor(level)), size = 3) + # nolint: object_usage_linter.
             ggplot2::scale_x_continuous("År", 2013:get_last_year(), limits = c(2013, get_last_year())) +
             ggplot2::scale_y_continuous("Oppgitt dekningsgrad", limits = c(0, 100)) +
             ggplot2::scale_colour_manual(values = colour_map, limits = c("A", "B", "C", NA)) +
