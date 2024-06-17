f36cd9585b42779e08a2cbb5b450fd3f321def88
stein-kato <steinkato.lindberg@yahoo.no>
2024-06-17 10:56:12 +0200

    Tabell med formatert data

--- R/mod_status.R
+++ R/mod_status.R
@@ -10,6 +10,31 @@
 #' @aliases status_ui status_server status_app
 NULL
 
+format_data <- function(dat) {
+  # Initialise data frame with column names
+  years <- sort(unique(dat$year))
+  registries <- sort(unique(dat$registry_id))
+  dat_format <- data.frame(matrix(ncol = length(years) + 1, nrow = length(registries)))
+  colnames(dat_format) <- c("Register", years)
+
+  dat_format$Register <- registries
+
+
+  for (i in seq_along(registries)) {
+    for (j in seq_along(years)) {
+      verdict <- dat[dat$registry_id == registries[i] & dat$year == years[j], ]
+
+      if (ncol(verdict) > 0 && nrow(verdict) > 0) {
+        dat_format[i, j + 1] <- verdict$verdict
+      } else {
+        dat_format[i, j + 1] <- NA
+      }
+    }
+  }
+
+  return(dat_format)
+}
+
 #' @rdname mod_status
 #' @export
 status_ui <- function(id) {
@@ -27,7 +52,11 @@ status_server <- function(id, pool, pool_verify) {
     function(input, output, session) {
       dat <- pool::dbGetQuery(pool, "SELECT registry_id, year, verdict FROM evaluation")
 
-      output$status_table <- DT::renderDataTable(dat)
+      rv = shiny::reactiveValues(
+        dat_format = format_data(dat)
+      )
+
+      output$status_table <- DT::renderDataTable(rv$dat_format)
     }
   )
 }
