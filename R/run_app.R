b3ea0fb10ed272e49772f5a9f26febb0ba22e986
arnfinn <arnfinn.steindal@gmail.com>
2024-08-08 15:45:12 +0200

    Åpne applikasjon i browser

--- R/run_app.R
+++ R/run_app.R
@@ -1,8 +1,8 @@
 #' Run the imongr Shiny Application
 #'
-#' @return An object representeing the imongr app
+#' @return An object representing the imongr app
 #' @export
 
 run_app <- function() {
-  shiny::shinyApp(ui = app_ui, server = app_server)
+  shiny::shinyApp(ui = app_ui, server = app_server, options = list(launch.browser = TRUE))
 }
