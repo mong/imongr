443b90be15f2ec28740c6ab0286b1c66edb28415
stein-kato <steinkato.lindberg@yahoo.no>
2026-09-08 08:48:46 +0200

    chore: lås Ekspertgruppens vurdering

--- R/mod_review.R
+++ R/mod_review.R
@@ -246,7 +246,7 @@ review_server <- function(id, registry_tracker, pool) {
 
     ########## HARDCODE EDIT LOCK ###########
     default_year <- 2025
-    disallow_edits <- FALSE
+    disallow_edits <- TRUE
     ########## HARDCODE EDIT LOCK ###########
 
     verdict <- shiny::reactive({
